0493aef15d2e49a138bea688b01b7369f01f7c94
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-03-30 17:38:03 -0400

    added page header component

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../_components/PageHeader'
+
+const AdminProductPage = () => {
+  return (
+   <>
+    <PageHeader>Products</PageHeader>
+   </>
+  )
+}
+
+export default AdminProductPage
